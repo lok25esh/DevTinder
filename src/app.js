42fdac8f733d90a2c5dde3bbc69f09cc6996bbbb
Lokesh Kallepalli <lokeshkallepalli2000@gmail.com>
2025-11-05 16:11:05 -0600

    setting middleware auth

--- src/app.js
+++ src/app.js
@@ -2,18 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use("/users", (req,res,next) => {
-    // res.send("1st response");
-    next();
-},
-(req,res,next) => {
-    // res.send("2nd response");
-    next();
-},
-(req,res)=> {
-    res.send("final response")
-}
-)
+app.use("/admin",(req,res,next)=> {
+    console.log("Authorization is done here")
+     const token = "xyzccd";
+     const isAdminAuth = token === "xyz"
+     if(!isAdminAuth){
+        res.status(401).send("Unauthorized");
+        
+     }
+     next();
+
+})
+
+app.get("/admin/getsData",(req,res)=> {
+    res.send("Admin gets the data ")
+})
 
 app.listen(3000,()=>{
     console.log("server is successfully listening on this port ")
