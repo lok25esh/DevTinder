3e0e7eac8eb2f394e469c85be3512b0804469b41
Lokesh Kallepalli <lokeshkallepalli2000@gmail.com>
2025-11-05 14:23:02 -0600

    multiple_route_handlers

--- src/app.js
+++ src/app.js
@@ -2,9 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test",(req,res) => {
-    res.send("hellow from the server ")
-})
+app.use("/users", (req,res,next) => {
+    // res.send("1st response");
+    next();
+},
+(req,res,next) => {
+    // res.send("2nd response");
+    next();
+},
+(req,res)=> {
+    res.send("final response")
+}
+)
 
 app.listen(3000,()=>{
     console.log("server is successfully listening on this port ")
